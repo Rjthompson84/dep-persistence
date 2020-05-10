893ed59fa20715b097200d665a69acd468439adb
Robert Thompson <robert.thompson@cognitran.com>
2020-05-10 20:56:20 +0100

    Init develop branch, add more basic tests for API endpoints.

--- src/test/kotlin/dep/persistence/api/EventAPIResourceTest.kt
+++ src/test/kotlin/dep/persistence/api/EventAPIResourceTest.kt
@@ -1,19 +1,54 @@
 package dep.persistence.api
 
+import dep.persistence.entities.User
+import dep.persistence.repository.UserRepository
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
+import io.restassured.http.Header
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty
+import java.time.LocalDateTime
+import javax.inject.Inject
 
 @QuarkusTest
 @DisabledIfSystemProperty(named = "profile", matches = "test")
 internal class EventAPIResourceTest {
+
+    @Inject private lateinit var userRepo: UserRepository
 
     @Test
     fun testGet() {
         given()
                 .`when`()
                 .get("/events/1/")
+                .then()
+                .statusCode(200)
+    }
+
+    @Test
+    fun testPost() {
+        var user : User = userRepo.save(User("dave", "bob", "email"))
+        val requestBodyString =
+                """
+                    { 
+                        "dateAndTime": "${LocalDateTime.now()}",
+                        "organiser" : { 
+                            "id":${user.id},
+                            "firstName":"${user.firstName}", 
+                            "lastName":"${user.lastName}", 
+                            "email":"${user.email}",
+                            "enabled":false
+                        },
+                        "title":"event",
+                        "venue":"venue"
+                    }
+                    """.trimIndent()
+
+        given()
+                .`when`()
+                .header(Header("Content-Type", "application/json"))
+                .body(requestBodyString)
+                .post("/events/")
                 .then()
                 .statusCode(200)
     }
